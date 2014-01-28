b626216b2e2f96cf3259ffad2c892352ab8b3454
ITJ6921 <ITJ6921@10.59.207.131>
2014-01-28 15:27:19 -0600

    refactor

--- com/src/main/java/sample/hello/resources/BookService.java
+++ com/src/main/java/sample/hello/resources/BookService.java
@@ -4,7 +4,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 @Path("/books")
 public class BookService {
@@ -14,11 +16,18 @@ public class BookService {
 	public Response getBooks(@PathParam("year") String year,
 			@MatrixParam("author") String author,
 			@MatrixParam("country") String country) {
-		return Response
-				.status(200)
-				.entity("getBooks is called, year : " + year
-						+ ", author : " + author + ", country : " + country)
-				.build();
+
+		CacheControl cacheControl = new CacheControl();
+		cacheControl.setMaxAge(86400);
+		cacheControl.setPrivate(true);
+		
+		ResponseBuilder builder = Response.status(200);
+		
+		builder.cacheControl(cacheControl);
+		builder.entity("getBooks is called, year : " + year + ", author : "
+				+ author + ", country : " + country);
+		Response response = builder.build();
+		return response;
 
 	}
 
--- com/src/main/java/sample/hello/resources/ContactsResource.java
+++ com/src/main/java/sample/hello/resources/ContactsResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
