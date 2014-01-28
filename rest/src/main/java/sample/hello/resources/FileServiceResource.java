1180eef849c588b168a8a89d749f135a969e356b
ITJ6921 <ITJ6921@10.59.207.131>
2014-01-28 08:23:02 -0600

    refactor

--- rest/src/main/java/sample/hello/resources/FileServiceResource.java
+++ rest/src/main/java/sample/hello/resources/FileServiceResource.java
@@ -16,11 +16,9 @@ public class FileServiceResource {
 	@GET
 	@Path("/get")
 	@Produces("text/plain")
-	public Response getFile() {
- 
+	public Response getFile() { 
 		File file = new File(FILE_PATH);
- 
-		ResponseBuilder response = Response.ok((Object) file);
+		ResponseBuilder response = Response.ok((Object)file);
 		response.header("Content-Disposition",
 			"attachment; filename=\"BahmanKalaliResume.docx\"");
 		return response.build();
